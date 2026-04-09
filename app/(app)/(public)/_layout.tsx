05ca68898141619a804b87a5b095888f00608c43
Benayo <ben_odeleye@yahoo.com>
2026-04-09 16:53:18 +0100

    feat: update project dependencies and scripts, remove reset-project script
    - Changed expo start commands to expo run for Android and iOS.
    - Added new dependencies: @expo-google-fonts/nunito, @expo/ui, @tanstack/react-query, expo-dev-client, expo-linear-gradient, haptics, nativewind, zustand.
    - Removed reset-project script and its associated file.
    - Updated tsconfig to include babel.config.js and nativewind-env.d.ts.
    - Created new layout and index files for authentication and public routes.
    - Added React Query provider for managing server state.
    - Configured Babel and Metro for NativeWind integration.
    - Added Tailwind CSS configuration and global styles.

---
+++ app/(app)/(public)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+export default function PublicLayout() {
+  return (
+    <Stack>
+      
+
+      <Stack.Screen name="index" options={{ headerShown: false , contentStyle: { backgroundColor: 'white' } }} />
+
+    </Stack>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import '../global.css';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Slot } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ReactQueryProvider from './providers/react-query-provider';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView className="flex-1">
+      <ReactQueryProvider>
+        <Slot />
+      </ReactQueryProvider>
+    </GestureHandlerRootView>
   );
 }
