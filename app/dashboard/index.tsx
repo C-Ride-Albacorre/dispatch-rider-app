d2a787ead100e60e34afbeebb1c53ef34045e5a1
Benayo <ben_odeleye@yahoo.com>
2026-04-16 18:30:41 +0100

    feat: add StatCard component for displaying dashboard statistics

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,7 +1,7 @@
-import { Colors, Fonts } from '@/constants/theme';
+import { Colors } from '@/constants/theme';
 import DashboardHeader from '@/features/dashboard/components/header';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Text, View } from 'react-native';
+import StatCard from '@/features/dashboard/components/stat-card';
+import { StyleSheet,  View } from 'react-native';
 
 export default function Dashboard() {
   return (
@@ -42,33 +42,6 @@ export default function Dashboard() {
   );
 }
 
-function StatCard({
-  title,
-  value,
-  icon,
-  iconBg = Colors.primary,
-  changeText = '+15%',
-}) {
-  return (
-    <View style={styles.statItem}>
-      <View style={styles.statHeader}>
-        <Text style={styles.statTitle}>{title}</Text>
-
-        <View style={[styles.statIcon, { backgroundColor: iconBg }]}>
-          <Ionicons name={icon} size={16} color={Colors.background} />
-        </View>
-      </View>
-
-      <Text style={styles.statValue}>{value}</Text>
-
-      <View style={styles.statChange}>
-        <Ionicons name="trending-up-outline" size={14} color={Colors.success} />
-        <Text style={styles.statChangeText}>{changeText}</Text>
-      </View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -83,65 +56,5 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'space-between',
     gap: 8,
-  },
-
-  // CARD
-  statItem: {
-    width: '48%',
-    backgroundColor: '#fff',
-    borderRadius: 14,
-    padding: 16,
-    gap: 12,
-
-    borderWidth: 1,
-    borderColor: Colors.border,
-
-    // shadow (iOS + Android)
-    shadowColor: '#000',
-    shadowOpacity: 0.05,
-    shadowRadius: 10,
-    shadowOffset: { width: 0, height: 4 },
-    elevation: 3,
-  },
-
-  // HEADER
-  statHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-
-  statTitle: {
-    fontSize: 13,
-    fontFamily: Fonts.brandRegular,
-    color: '#666',
-  },
-
-  statIcon: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  // VALUE
-  statValue: {
-    fontSize: 22,
-    fontFamily: Fonts.brandBold,
-    color: Colors.text,
-  },
-
-  // CHANGE ROW
-  statChange: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 4,
-  },
-
-  statChangeText: {
-    fontSize: 12,
-    fontFamily: Fonts.brandRegular,
-    color: Colors.success,
   },
 });
---
+++ features/dashboard/components/stat-card.tsx
@@ -0,0 +1,100 @@
+import { Colors, Fonts } from '@/constants/theme';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function StatCard({
+  title,
+  value,
+  icon,
+  iconBg = Colors.primary,
+  changeText = '+15%',
+}: {
+  title: string;
+  value: string;
+  icon: keyof typeof Ionicons.glyphMap;
+  iconBg?: string;
+  changeText?: string;
+}) {
+  return (
+    <View style={styles.statItem}>
+      <View style={styles.statHeader}>
+        <Text style={styles.statTitle}>{title}</Text>
+
+        <View style={[styles.statIcon, { backgroundColor: iconBg }]}>
+          <Ionicons name={icon} size={16} color={Colors.background} />
+        </View>
+      </View>
+
+      <Text style={styles.statValue}>{value}</Text>
+
+      <View style={styles.statChange}>
+        <Ionicons name="trending-up-outline" size={14} color={Colors.success} />
+        <Text style={styles.statChangeText}>{changeText}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+
+  // CARD
+  statItem: {
+    width: '48%',
+    backgroundColor: '#fff',
+    borderRadius: 14,
+    padding: 16,
+    gap: 12,
+
+    borderWidth: 1,
+    borderColor: Colors.border,
+
+    // shadow (iOS + Android)
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 3,
+  },
+
+  // HEADER
+  statHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  statTitle: {
+    fontSize: 13,
+    fontFamily: Fonts.brandRegular,
+    color: '#666',
+  },
+
+  statIcon: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  // VALUE
+  statValue: {
+    fontSize: 22,
+    fontFamily: Fonts.brandBold,
+    color: Colors.text,
+  },
+
+  // CHANGE ROW
+  statChange: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+
+  statChangeText: {
+    fontSize: 12,
+    fontFamily: Fonts.brandRegular,
+    color: Colors.success,
+  },
+});
